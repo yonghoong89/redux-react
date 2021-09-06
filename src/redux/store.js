4174cb9d689504d8e0b0a5871b511fab03dcd4f7
unknown <yonghoong89@homeplus.co.kr>
2021-09-06 16:27:58 +0900

    redux thunk 사용

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer';
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
 
-const middleware = [logger]
+const middleware = [logger, thunk]
 const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store
