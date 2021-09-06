86f601d8c64e643d73d0679d99c1a99ed166843b
unknown <yonghoong89@homeplus.co.kr>
2021-09-06 16:16:20 +0900

    redux extention 사용

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './rootReducer';
 import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const middleware = [logger]
-const store = createStore(rootReducer,applyMiddleware(...middleware))
+const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(...middleware)))
 
 export default store
